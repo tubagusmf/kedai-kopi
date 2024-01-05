dab94fe051f4fae5d8f92ae3ec25ddc60b05cba5
Tubagus M Fadillah <tubagusfadillah98@gmail.com>
2024-01-05 10:44:07 +0700

    membuat website kedai kopi sederhana

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//Toggle Class Active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+//ketika humberger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+}
+
+//klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove('active'); 
+    }
+});
